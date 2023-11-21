4b7d45e99332828da2d64ee0847421d75595f5af
yulanh7 <yulanh7@gmail.com>
2023-11-21 02:19:24 +0000

    add language attribute

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,20 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Provider } from 'react-redux';
 import store from '../store';
 import Router from 'next/router';
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { useRouter } from 'next/router';
+
 config.autoAddCss = false
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Add the lang attribute to the <html> tag when the component mounts
+    document.documentElement.lang = router.locale || 'en-AU';
+  }, [router.locale]);
+
   useEffect(() => {
     const handleRouteChange = (url: string) => {
       // Set the current URL as the previous URL
